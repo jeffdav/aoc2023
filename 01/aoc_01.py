34013df4abe9572a5d0d255eb06fcdbddcde41ca
Jeff <jeffdav@users.noreply.github.com>
2023-11-30 23:41:24 -0800

    AoC #01.

--- 01/aoc_01.py
+++ 01/aoc_01.py
@@ -16,8 +16,9 @@ def part1():
 
 def part2():
   nums = "0123456789"
-  alpha_nums = {"one": 1, "two": 2, "three": 3, "four": 4,
-                "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+  alpha_nums = { "one": 1, "two": 2, "three": 3,
+                 "four": 4, "five": 5, "six": 6,
+                 "seven": 7, "eight": 8, "nine": 9 }
   sum = 0
 
   with open('input_01.txt', 'r') as file:
