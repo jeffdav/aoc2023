ec56c771c883d045e42aa0161b20ed26ff9d94af
Jeff <jeffdav@users.noreply.github.com>
2023-12-03 21:22:35 -0800

    AoC #03.

--- 03/aoc_03.py
+++ 03/aoc_03.py
@@ -45,9 +45,68 @@ def part1():
 
     print("Part 1: " + str(sum))
 
+def find_number(line, i):
+  start = i
+  while start >= 0 and line[start - 1].isdigit():
+    start -= 1
+
+  end = i
+  while end < len(line) - 1 and line[end + 1].isdigit():
+    end += 1
+
+  num = 0
+  for c in line[start:end+1]:
+    num = 10 * num + int(c)
+  return num
+
+def part2():
+  with open('03/input_03.txt', 'r') as file:
+    lines = file.readlines()
+    line_num = 0
+    sum = 0
+    while line_num < len(lines):
+      prev_line = lines[line_num - 1].rstrip() if line_num > 0 else None
+      curr_line = lines[line_num].rstrip()
+      next_line = lines[line_num + 1].rstrip() if line_num < len(lines) - 1 else None
+      i = 0
+      nums = []
+      while i < len(curr_line):
+        if curr_line[i] == '*':
+          if prev_line:
+            if prev_line[i].isdigit():
+              nums.append(find_number(prev_line, i))
+            else:
+              if i > 0 and prev_line[i-1].isdigit():
+                nums.append(find_number(prev_line, i-1))
+              if i < len(prev_line) - 1 and prev_line[i+1].isdigit():
+                nums.append(find_number(prev_line, i+1))
+          if i > 0 and curr_line[i-1].isdigit():
+            nums.append(find_number(curr_line, i-1))
+          if i < len(curr_line) - 1 and curr_line[i+1].isdigit():
+            nums.append(find_number(curr_line, i+1))
+          if next_line:
+            if next_line[i].isdigit():
+              nums.append(find_number(next_line, i))
+            else:
+              if i > 0 and next_line[i-1].isdigit():
+                nums.append(find_number(next_line, i-1))
+              if i < len(next_line) - 1 and next_line[i+1].isdigit():
+                nums.append(find_number(next_line, i+1))
+
+          if len(nums) == 2:
+            print("YES: " + str(nums))
+            sum += (nums[0] * nums[1])
+          else:
+            print("NO: " + str(nums))
+          nums = []
+        i += 1
+      line_num += 1
+
+  print("Part 2: " + str(sum))
 
 def main():
   part1()
+  part2()
 
 if __name__ == "__main__":
   main()
